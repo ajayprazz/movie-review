19cb0e66f9f0ae35960938fee62458943dfe1cdf
Ajay Prajapati <ajayprazz@gmail.com>
2020-06-22 07:46:58 +0545

    moved db config to models

--- controllers/genre.js
+++ controllers/genre.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require("./../config/db");
+const db = require("./../models/");
 const authorization = require('../middlewares/authorization');
 
 module.exports = {
--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const config = require("./../config");
-const db = require("./../config/db");
+const db = require("./../models/");
 
 module.exports = (req, res, next) => {
     let token;
